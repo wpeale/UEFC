3cda2683b9559255a903175af97c18b9ec660f5e
Kristoff1318 <kristoff.misquitta@gmail.com>
2023-03-18 13:49:29 -0400

    Data and contour plots for q 4,5,6

---
+++ Q4_5_6/contour.py
@@ -0,0 +1,38 @@
+import json
+import numpy as np
+import matplotlib.pyplot as plt
+import math
+
+def gen_graph(file, title):
+    with open(file) as f:
+        data = f.read()
+
+    js = json.loads(data)
+    lamb_dup = [js[k][0][3] for k in js]
+    tau_dup = np.array([js[k][0][2] for k in js])
+    tau = np.unique(tau_dup)
+    lamb  = np.unique(lamb_dup)
+    L, T = np.meshgrid(lamb, tau)
+
+    mO3 = np.ndarray((15,15))
+    for t in range(15):
+        ta = tau[t]
+        for l in range(15):
+            la = lamb[l]
+            for k in js:
+                if math.isclose(js[k][0][3],la) and math.isclose(js[k][0][2],ta):
+                    mO3[l][t] = float(k)
+
+    _, ax = plt.subplots()
+    levels = np.linspace(np.min(mO3[mO3 > 0]), np.max(mO3), 21)
+    cs = ax.contour(T, L, mO3, levels=levels, linewidths=0.5)
+    plt.clabel(cs, inline=1, fontsize=12)
+    ax.set_xlabel(r'$\tau$')
+    ax.set_ylabel('$\lambda$')
+    ax.set_title(title)
+    plt.show()
+
+
+gen_graph('Optimal_ARS_3.txt', '$m_{\mathrm{pay}}\Omega^{3}$ Contour Plot (Question 4)')
+gen_graph('Optimal_ARS_2.txt', '$m_{\mathrm{pay}}\Omega^{3}$ Contour Plot (Question 5)')
+gen_graph('Optimal_ARS_1.txt', '$m_{\mathrm{pay}}\Omega^{3}$ Contour Plot (Question 6)')
